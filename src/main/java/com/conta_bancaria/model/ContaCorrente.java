185224a7bf5c260f6fb9312893e3b56ebe4bea1a
unknown <josadaqueferreira@outlook.com>
2024-12-21 00:56:37 -0300

    ":sparkles: Implementação das classes ContaCorrente e ContaPoupanca"
    This reverts commit c7a886463a7fcec1f5337d6bdc62832d58d0ed54.

---
+++ src/main/java/com/conta_bancaria/model/ContaCorrente.java
@@ -0,0 +1,31 @@
+package com.conta_bancaria.model;
+
+import jakarta.persistence.DiscriminatorValue;
+import jakarta.persistence.Entity;
+
+import java.util.UUID;
+
+@Entity
+@DiscriminatorValue("Corrente") // Valor que será armazenado na coluna do discriminator
+public class ContaCorrente extends Conta{
+
+    private double _limite;
+
+    public ContaCorrente() {
+        super();  // Necessário para chamadas ao construtor da superclasse
+    }
+
+    public ContaCorrente(UUID numero, int agencia, int tipo, String titular, int saldo, double limite) {
+        super(numero, agencia, tipo, titular, saldo);
+        this._limite = limite;
+    }
+
+
+    public double get_limite() {
+        return _limite;
+    }
+
+    public void set_limite(double limite) {
+        this._limite = limite;
+    }
+}
